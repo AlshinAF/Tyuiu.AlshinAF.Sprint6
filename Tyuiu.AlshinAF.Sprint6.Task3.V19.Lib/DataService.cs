74533f3b9487e4f8c4eba6b5bcb56c76fbe01440
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-12-11 02:03:37 +0500

    Task3

--- Tyuiu.AlshinAF.Sprint6.Task3.V19.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint6.Task3.V19.Lib/DataService.cs
@@ -5,22 +5,22 @@ namespace Tyuiu.AlshinAF.Sprint6.Task3.V19.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            var rows = Enumerable.Range(0, 5).Select(i => (row: i, value: matrix[i, 4])).ToArray();
-
-            // Sort the tuples based on the 5th column values.
-            Array.Sort(rows, (a, b) => a.value.CompareTo(b.value));
-
-            // Create a new matrix with sorted rows.
-            int[,] sortedMatrix = new int[5, 5];
+            int[] fifthColumn = new int[5];
             for (int i = 0; i < 5; i++)
             {
-                for (int j = 0; j < 5; j++)
-                {
-                    sortedMatrix[i, j] = matrix[rows[i].row, j];
-                }
+                fifthColumn[i] = matrix[i, 4];
             }
 
-            return sortedMatrix;
+            // Sort the fifth column
+            Array.Sort(fifthColumn);
+
+            // Update the fifth column in the matrix
+            for (int i = 0; i < 5; i++)
+            {
+                matrix[i, 4] = fifthColumn[i];
+            }
+
+            return matrix;
 
         }
     }
