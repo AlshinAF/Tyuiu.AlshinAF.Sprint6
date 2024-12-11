cb660eb7fce0050b555b5935541e5148ecd6512c
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-12-11 22:06:42 +0500

    Task4

---
+++ Tyuiu.AlshinAF.Sprint6.Task4.V19/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.AlshinAF.Sprint6.Task4.V19
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
