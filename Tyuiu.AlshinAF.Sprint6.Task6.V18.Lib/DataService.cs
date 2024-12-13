756bcbbde5d49c12956b3b2d8a22e7a38110b61f
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-12-14 04:16:18 +0500

    Task6

--- Tyuiu.AlshinAF.Sprint6.Task6.V18.Lib/DataService.cs
+++ Tyuiu.AlshinAF.Sprint6.Task6.V18.Lib/DataService.cs
@@ -3,11 +3,13 @@ namespace Tyuiu.AlshinAF.Sprint6.Task6.V18.Lib
 {
     public class DataService : ISprint6Task6V18
     {
-        public string CollectTextFromFile(string str, string path)
+        public string CollectTextFromFile(string path)
         {
+            string str = "";
             string resStr = "";
             using (StreamReader reader = new StreamReader(path))
             {
+                
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
